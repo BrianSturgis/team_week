2f0499b44ee09121186ab3a97e6303e5a9f89adc
Max Brockbank <maxbrockbank1999@gmail.com>
2020-11-30 15:31:22 -0800

    save img url in variablefor transforming
    Co-Authored-By: Brian Sturgis <sturujisu@gmail.com>

--- src/compressAPI.js
+++ src/compressAPI.js
@@ -1,13 +1,15 @@
 import {Cloudinary} from "cloudinary-core";
 
 export default function openWidget(){
+  let img;
 	let myWidget = cloudinary.createUploadWidget({
 		cloudName:`${process.env.CLOUD_NAME}`,
 		uploadPreset: `${process.env.UPLOAD_PRESET}`
 	}, (error, result) => {
 		if(!error && result && result.event === "success"){
-			console.log('Done! Here is the image info: ', result.info);
+      console.log('Done! Here is the image info: ', result.info);
+      img = result.info.secure_url;
 		}
 	});
-	myWidget.open();
+  myWidget.open();
 }
