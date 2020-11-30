d4b3e75603674bba17d68d1b506829853e9f55f1
Chelsea Becker <cschweig2@gmail.com>
2020-11-30 15:47:39 -0800

    add basic UI to index.html

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
+$(document).ready(function() {
+  $("form#submit").submit(function(event) {
+  event.preventDefault();
+  const img = $("input#myFile").val();
+  console.log(`${img}`);
+  });
+});
