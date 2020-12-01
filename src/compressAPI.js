5b0ea67308246f96eac2827d17b65afbc83e87e4
Max Brockbank <maxbrockbank1999@gmail.com>
2020-12-01 11:25:03 -0800

    successfully compress images
    Co-Authored-By: Brian Sturgis <sturujisu@gmail.com>

--- src/compressAPI.js
+++ src/compressAPI.js
@@ -1,15 +1,26 @@
 import {Cloudinary} from "cloudinary-core";
 
-export default function openWidget(){
-  let img;
+// Set config parameters while instantiating a new Cloudinary class
+let cl = new Cloudinary({cloud_name:`${process.env.CLOUD_NAME}`, api_key:`${process.env.COMPRESS_KEY}`, api_secret:`${process.env.COMPRESS_SECRET}`, secure: true});
+
+// Function to open widget to upload file to cloudinary, returns file url
+export function openWidget(){
 	let myWidget = cloudinary.createUploadWidget({
 		cloudName:`${process.env.CLOUD_NAME}`,
 		uploadPreset: `${process.env.UPLOAD_PRESET}`
 	}, (error, result) => {
 		if(!error && result && result.event === "success"){
-      console.log('Done! Here is the image info: ', result.info);
-      img = result.info.secure_url;
+			console.log('Done! Here is the image info: ', result.info);
+			imgTransform(result.info);
 		}
 	});
-  myWidget.open();
+	myWidget.open();
 }
+
+// Function to transform img aspect ratio and web opitmization 
+function imgTransform(url){
+	console.log(url.secure_url, url.bytes);
+	const compressed = cl.imageTag(`${url.public_id}.jpg`, {quality: "auto"}).toHtml();
+	console.log(compressed);
+}
+// Function to display the change in file size to DOM
--- src/main.js
+++ src/main.js
@@ -2,21 +2,14 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import openWidget from './compressAPI.js';
-
+import {openWidget} from './compressAPI.js';
 
 
 // Store HTML File Input in Variable
 $(document).ready(function(){
 	$("#widget").append("<script src ='https://widget.cloudinary.com/v2.0/global/all.js' type'text/javascript'></script>");
-	$("#open-widget").on('click', function(){
-		openWidget();
+	$("#open-widget").on('click', async function(){
+		const result = openWidget();
 	});
-	// $("#upload-form").submit(function(event){
-	//   event.preventDefault();
-	//   const input = $("#upload").val();
-	//   console.log(input);
-	//   newUpload.v2.uploader.upload(input, function(error, result){console.log(result, erorr)});
-	// });
   
 });
